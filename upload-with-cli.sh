fd64ccb2592f2f53c64938a0c051b46be5fad261
joostboon <joostboon@gmail.com>
2024-02-22 17:02:57 +0100

    Update upload-with-cli.sh

--- upload-with-cli.sh
+++ upload-with-cli.sh
@@ -1,5 +1,1 @@
-curl -X POST -F labuitslag=@/home/stephan.vanderzwaard@mydre.org/public/labuitslag.csv \
--F medicatie_toediening=@@/home/stephan.vanderzwaard@mydre.org/public/medicatie_toediening.csv \
--F medische_diagnose=@/home/stephan.vanderzwaard@mydre.org/public/medische_diagnose.csv \
--F opnametraject=@/home/stephan.vanderzwaard@mydre.org/public/opnametraject.csv \
-"http://127.0.0.1:5001/upload-files-cli"
+curl -X POST -F labuitslag=@/home/stephan.vanderzwaard@mydre.org/public/labuitslag.csv -F medicatie_toediening=@@/home/stephan.vanderzwaard@mydre.org/public/medicatie_toediening.csv -F medische_diagnose=@/home/stephan.vanderzwaard@mydre.org/public/medische_diagnose.csv -F opnametraject=@/home/stephan.vanderzwaard@mydre.org/public/opnametraject.csv "http://127.0.0.1:5001/upload-files-cli"
