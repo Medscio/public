0ad74e3bd0664941ff464c860e817317da156db0
joostboon <joostboon@gmail.com>
2024-02-23 10:11:14 +0100

    Update upload-with-cli.sh

--- upload-with-cli.sh
+++ upload-with-cli.sh
@@ -1,6 +1,5 @@
 curl -X POST \
 -F labuitslag=@labuitslag.csv \
--F medicatie=@medicatie.csv \
 -F medicatie_toediening=@medicatie_toediening.csv \
 -F medische_diagnose=@medische_diagnose.csv \
 -F opnametraject=@opnametraject.csv \
