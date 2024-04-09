a9e8295c78e35a18ce7dbad18da40ad4218bd869
joostboon <joostboon@gmail.com>
2024-04-09 15:38:30 +0200

    added copy log line

--- cp-from-docker.sh
+++ cp-from-docker.sh
@@ -1,1 +1,2 @@
+sudo docker exec $(sudo docker ps --filter name=app -q) bash -c "mkdir -p /extract; cp -f /app/*.log /app/results"
 sudo docker cp $(sudo docker ps --filter name=app -q):/app/results results
