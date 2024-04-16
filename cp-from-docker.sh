c21743fa302e18a0d3d584df6714a2ae4decc02e
joostboon <joostboon@gmail.com>
2024-04-16 10:43:38 +0200

    small update

--- cp-from-docker.sh
+++ cp-from-docker.sh
@@ -1,2 +1,3 @@
-sudo docker exec $(sudo docker ps --filter name=app -q) bash -c "mkdir -p /extract; cp -f /app/*.log /app/results"
-sudo docker cp $(sudo docker ps --filter name=app -q):/app/results results
+sudo docker exec $(sudo docker ps --filter name=app -q) bash -c "mkdir -p /results; cp -f /app/*.log /app/results"
+sudo rm -rf ./results
+sudo docker cp $(sudo docker ps --filter name=app -q):/app/results ./results
