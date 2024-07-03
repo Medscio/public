c516c50f4d66a7af4aa17ed3e4b785502a61daba
joostboon <joostboon@gmail.com>
2024-07-03 21:20:08 +0200

    updated yml files

--- upload-with-cli.sh
+++ upload-with-cli.sh
@@ -4,11 +4,12 @@ start_date=$(date +"%Y-%m-%d %H:%M:%S")
 echo "Start Time: $start_date"
 
 curl -X POST -F labuitslag=@labuitslag.csv -F medicatie_toediening=@medicatie_toediening.csv -F medische_diagnose=@medische_diagnose.csv -F opnametraject=@opnametraject.csv "127.0.0.1:5555/upload-files-cli"
+
 end_time=$(date +%s)
 
 end_date=$(date +"%Y-%m-%d %H:%M:%S")
 echo "End Time: $end_date"
 
 total_time=$((end_time - start_time))
-total_time_formatted=$(date -ud "@$total_time" +"%H:%M:%S")
-echo "Total Time Taken: $total_time_formatted seconds"
+total_time_formatted=$(printf '%02d:%02d:%02d\n' $(($total_time/3600)) $(($total_time%3600/60)) $(($total_time%60)))
+echo "Total Time Taken: $total_time_formatted"
