53c486641ab6ddabdbdca3648d2ab8dad0f1d22c
joostboon <joostboon@gmail.com>
2024-07-12 11:39:17 +0200

    updated docker and new version

--- run_docker-multi.sh
+++ run_docker-multi.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-# Check if the user is already logged in to Azure
-if az account show > /dev/null 2>&1; then
-  echo "Already logged in to Azure."
-else
-  # Prompt the user for password securely
-  read -s -p "Enter your password: " PASSWORD
+# Prompt the user for password securely
+read -s -p "Enter your password: " PASSWORD
 
-  # Login to Azure
-  if ! sudo az login -u amsterdamumc@medscio.nl -p "$PASSWORD"; then
-    echo "Login failed. Please check your credentials."
-    exit 1
-  fi
-fi
+# Login to Azure
+sudo az login -u amsterdamumc@medscio.nl -p "$PASSWORD"
 
 # Login to Azure Container Registry
-if ! sudo az acr login --name medscio; then
-  echo "Failed to log in to Azure Container Registry."
-  exit 1
-fi
+sudo az acr login --name medscio
 
 # Start Docker Compose
 sudo docker-compose -f docker-compose-multi.yml up --force-recreate
