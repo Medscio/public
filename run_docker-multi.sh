a05af98a731a5f9b0154594e120170f440f240d9
joostboonzajer <joostboon@gmail.com>
2024-08-08 11:14:35 +0200

    small updates and added volume

--- run_docker-multi.sh
+++ run_docker-multi.sh
@@ -9,5 +9,7 @@ sudo az login -u amsterdamumc@medscio.nl -p "$PASSWORD"
 # Login to Azure Container Registry
 sudo az acr login --name medscio
 
+sudo docker rm $(sudo docker ps -a -q)
+
 # Start Docker Compose
-sudo docker-compose -f docker-compose-multi.yml up -d --force-recreate
+sudo docker-compose -f docker-compose-multi.yml up --force-recreate
